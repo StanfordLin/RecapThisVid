4749544427f32ce4f9a9da521fdb988717a05e93
Stanford Lin <stanlin1999@gmail.com>
2020-03-28 23:56:49 -0700

    full text from VI api

--- Backend/VideoIntelligence.py
+++ Backend/VideoIntelligence.py
@@ -5,6 +5,7 @@ from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
 from datetime import datetime
+from google.cloud import videointelligence
 
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
@@ -71,6 +72,57 @@ def transcribe_video(url):
 '''
 paragraph = f.read()
 '''
+
+def transcribe_get_all(url):
+    startTime = datetime.now()
+    """Transcribe speech from a video stored on GCS."""
+    video_client = videointelligence.VideoIntelligenceServiceClient()
+    features = [videointelligence.enums.Feature.SPEECH_TRANSCRIPTION]
+
+    config = videointelligence.types.SpeechTranscriptionConfig(
+        language_code="en-US", enable_automatic_punctuation=True
+    )
+    video_context = videointelligence.types.VideoContext(
+        speech_transcription_config=config
+    )
+
+    operation = video_client.annotate_video(
+        "gs://videos12491/trimmed.mp4", features=features, video_context=video_context
+    )
+
+    print("\nProcessing video for speech transcription.")
+
+    result = operation.result(timeout=600)
+
+    # There is only one annotation_result since only
+    # one video is processed.
+    annotation_results = result.annotation_results[0]
+    for speech_transcription in annotation_results.speech_transcriptions:
+        # print(speech_transcription)
+
+        # The number of alternatives for each transcription is limited by
+        # SpeechTranscriptionConfig.max_alternatives.
+        # Each alternative is a different possible transcription
+        # and has its own confidence score.
+        for alternative in speech_transcription.alternatives:
+            print("Alternative level information:")
+
+            print("Transcript: {}".format(alternative.transcript))
+            print("Confidence: {}\n".format(alternative.confidence))
+
+            print("Word level information:")
+            for word_info in alternative.words:
+                word = word_info.word
+                start_time = word_info.start_time
+                end_time = word_info.end_time
+                print(
+                    "\t{}s - {}s: {}".format(
+                        start_time.seconds + start_time.nanos * 1e-9,
+                        end_time.seconds + end_time.nanos * 1e-9,
+                        word,
+                    )
+                )
+    # print(f"""Execution Time: {datetime.now() - startTime}""")
 
 def split_text_into_sentences():
     sentences = paragraph.split(".")
@@ -151,6 +203,6 @@ def generate_summary():
 
 if __name__ == "__main__":
     url = ""
-    transcribe_video(url)
+    transcribe_get_all(url)
     # generate_summary()
     # print("Paragraph summarized: " + paragraph)
