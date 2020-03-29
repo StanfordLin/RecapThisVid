e49cc7d0bd156bb4c0555cbee205df7f6e52f7ff
Stanford Lin <stanlin1999@gmail.com>
2020-03-29 01:19:52 -0700

    added pubsub and email cloud functions

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,11 @@
 from flask import escape
 from google.cloud import videointelligence
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import Credentials
 
+SendGridAPIKey = Credentials.SendGridAPIKey
+error_count = 0
 
 def hello_http(request):
     """HTTP Cloud Function.
@@ -15,10 +20,63 @@ def hello_http(request):
     request_json = request.get_json(silent=True)
     request_args = request.args
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
+    if request_json and 'url' in request_json:
+        url = request_json['url']
+    elif request_args and 'url' in request_args:
+        url = request_args['url']
+    else:
+        url = 'World'
+    return 'Hello {}!'.format(escape(url))
+
+def hello_pubsub(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+    """
+    import base64
+
+    print("""This Function was triggered by messageId {} published at {}
+    """.format(context.event_id, context.timestamp))
+
+    if 'data' in event:
+        name = base64.b64decode(event['data']).decode('utf-8')
     else:
         name = 'World'
-    return 'Hello {}!'.format(escape(name))
+    print('Hello {}!'.format(name))
+    formulate_message("stanlin1999@gmail.com","Test","Message from cloud function: " + event['data'])
+
+def formulate_message(email, message, url):
+    email = Mail(
+        from_email='nihalpot@gmail.com',
+        to_emails=email,
+        subject=('A summary of the following video: '+str(url)),
+        html_content='<strong>'+message+'</strong>') 
+    return send_email(email)
+
+def send_email(email):
+    global error_count
+    try:
+        sg = SendGridAPIClient(SendGridAPIKey) # create the sendgrid client
+        # print out the http response code and characteristics
+        response = sg.send(email) 
+        print(response.status_code)
+
+        if response.status_code != 202: # retry three times until this works
+            if error_count < 3:
+                error_count = error_count+1
+                send_email(email)
+            else:
+                return "Failure."
+
+        return "Success."
+    except Exception as e:
+        print(e)
+        if error_count < 3:
+            error_count = error_count+1
+            send_email(email)
+        return "Failure."
